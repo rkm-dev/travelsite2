c670f655bff88a1351c45208af5e8ab660a6e8ad
Rahul <k.m_rahul@yahoo.com>
2017-11-25 18:46:34 +0530

    Added Support for Legacy Browsers

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
 require('./gulp/tasks/sprites');
 require('./gulp/tasks/scripts');
+require('./gulp/tasks/modernizr');
 
 // gulp.task('default', function(){
 //   console.log("Gulp task initiated...");
